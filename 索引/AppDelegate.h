f55c7cd506b855c44652822cebb35540dd33507f
rsxx <rsxx@MacBook-Air.local>
2018-01-08 14:06:35 +0800

    Initial Commit

---
+++ 索引/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  索引
+//
+//  Created by rsxx on 2018/1/8.
+//  Copyright © 2018年 rsxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 索引/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  索引
+//
+//  Created by rsxx on 2018/1/8.
+//  Copyright © 2018年 rsxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
